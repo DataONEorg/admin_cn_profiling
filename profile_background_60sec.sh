272a5036eca58308cf0cf47bc323b17b896f15fe
datadavev <datadavev@users.noreply.github.com>
2018-11-16 11:25:03 -0800

    generalized

--- profile_background_60sec.sh
+++ profile_background_60sec.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-DEST_FN="prof_background_60.svg"
+DURATION="120"
+DEST_FN="prof_background_${DURATION}.svg"
 BASE_URL="https://cn-unm-1.dataone.org/cn/v2/meta/"
 PROFILER="./profiler.sh"
 TOMCAT_PID=$(sudo -u tomcat7 jps | grep "Bootstrap" | awk '{print $1}')
 echo "Start profiling jps id: ${TOMCAT_PID}"
-sudo -u tomcat7 ${PROFILER} -d 60 -f "/tmp/${DEST_FN}" --title "background_60" ${TOMCAT_PID}
+sudo -u tomcat7 ${PROFILER} -d ${DURATION} -f "/tmp/${DEST_FN}" --title "background_${DURATION}" --width 2000 ${TOMCAT_PID}
 echo "Done profiling jps id: ${TOMCAT_PID}"
 cp "/tmp/${DEST_FN}" "/var/www/profiling/${DEST_FN}"
