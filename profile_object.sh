d5d1e7c0e3f036317efe4cb7b6ea8d032f5fb8ce
datadavev <datadavev@users.noreply.github.com>
2018-11-20 06:41:35 -0500

    updates

--- profile_object.sh
+++ profile_object.sh
@@ -11,7 +11,7 @@ sudo -u tomcat7 ${PROFILER} start -f "/tmp/${DEST_FN}" ${TOMCAT_PID}
 cat ${PID_FILE} | while read PID; do
   URL="${BASE_URL}${PID}"
   echo ${URL}
-  curl -s ${URL}
+  curl -s ${URL} > /dev/null
 done
 sudo -u tomcat7 ${PROFILER} stop -f "/tmp/${DEST_FN}" --title "getObject" ${TOMCAT_PID}
 echo "Done profiling jps id: ${TOMCAT_PID}"
--- profile_view.sh
+++ profile_view.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SPATH=$(dirname ${0})
-DEST_FN="prof_view_100.svg"
+DEST_FN=${1}
+#DEST_FN="prof_view_100.svg"
 PID_FILE="${SPATH}/iso_pangaea_pids.txt"
 BASE_URL="https://cn-unm-1.dataone.org/cn/v2/views/metacatui/"
 PROFILER="./profiler.sh"
@@ -11,7 +12,7 @@ sudo -u tomcat7 ${PROFILER} start -f "/tmp/${DEST_FN}" ${TOMCAT_PID}
 cat ${PID_FILE} | while read PID; do
   URL="${BASE_URL}${PID}"
   echo ${URL}
-  curl -s ${URL}
+  curl -s ${URL} >/dev/null
 done
 sudo -u tomcat7 ${PROFILER} stop -f "/tmp/${DEST_FN}" --title "views metacatui" ${TOMCAT_PID}
 echo "Done profiling jps id: ${TOMCAT_PID}"
